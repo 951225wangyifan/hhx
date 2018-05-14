eefc78ee3053cf1b2112ec3033a2b6da720a5eaf
wyf <1284828673@qq.com>
2018-05-14 10:40:09 +0800

    crontab

---
+++ Application/Admin/Controller/CrontabController.class.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/14
+ * Time: 10:30
+ */
+namespace Admin\Controller;
+use Admin\Controller\CheckController;
+class CrontabController extends CheckController {
+
+    /* public function __construct(){
+        parent::__construct();
+    }
+     */
+    public function index(){
+        $data['title'] = 'test';
+        $data['content'] = '测试crontab内容';
+        $data['type'] = range(1,4);
+        $data['create_time'] = time();
+        M('news')->add();
+        $this->display();
+    }
+}
